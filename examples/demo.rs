38bbd454562984c5e71435b9fa36808609cc87e6
Peter Holloway <peter.holloway@diamond.ac.uk>
2021-10-27 13:34:58 +0100

    Add CLI arg handling to read from file or server

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,23 +1,32 @@
-use std::{fs::File, net::{SocketAddr, TcpStream, ToSocketAddrs}, thread::{self, Thread}, time::Duration};
+use gumdrop::Options;
+use std::{fs::File, io::Read, net::TcpStream, path::PathBuf, thread, time::Duration};
 
 use logback::LogLevel;
 
 pub fn main() {
-    // let src = loop {
-        // if let Ok(sock) = TcpStream::connect(("localhost", 6750)) {
-        //     break sock;
-        // }
-        // thread::sleep(Duration::from_millis(200));
-    // };
-    // println!("Connected to server");
-    let src = File::open("/tmp/i22_log.log").unwrap();
+    let command = Command::parse_args_default_or_exit();
+
+    let src: Box<dyn Read> = if let Some(file) = command.file {
+        Box::new(File::open(file).unwrap())
+    } else {
+        let host = command.host.as_deref().unwrap_or("localhost");
+        let port = command.port.unwrap_or(6750);
+        loop {
+            if let Ok(sock) = TcpStream::connect((host, port)) {
+                println!("Connected to server");
+                break Box::new(sock);
+            }
+            thread::sleep(Duration::from_millis(200));
+        }
+    };
+
     let mut reader = jaded::Parser::new(src).expect("failed to create parser");
 
     let mut count = 0;
     loop {
         let evt = reader.read().unwrap();
         use jaded::FromJava;
-        match logback::LogEvent::from_value(evt.value()){
+        match logback::LogEvent::from_value(evt.value()) {
             Ok(evt) => {
                 if evt.level >= LogLevel::Info {
                     println!("{}", evt.message());
@@ -27,10 +36,21 @@ pub fn main() {
                     println!("Read {} messages", count);
                     break;
                 }
-            },
+            }
             Err(e) => {
                 println!("{}", e);
             }
         }
     }
+}
+
+#[derive(Debug, Default, Options)]
+struct Command {
+    #[options(help = "Read log messages from file")]
+    file: Option<PathBuf>,
+    #[options(help = "Connect to server to read messages")]
+    host: Option<String>,
+    #[options(help = "Server port broadcasting log messages - default: 6750")]
+    port: Option<u16>,
+    startup: bool,
 }
